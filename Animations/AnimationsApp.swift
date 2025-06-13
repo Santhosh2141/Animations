6473e7fc288fdee93d729ca0fc70b82af70fe53a
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-13 19:47:02 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Santhosh Srinivas on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
